b5b77ca83241867ae0dc6608e46c70ce12df1aa9
João Nogueira <shires.waking0d@icloud.com>
2024-03-24 13:00:18 +0100

    Expanded outputs

--- flake.nix
+++ flake.nix
@@ -1,1 +1,14 @@
-{ outputs = { ... }: (builtins.fromJSON (builtins.readFile ./registry.json)); }
+{
+  outputs = { ... }:
+    let
+      raw = builtins.readFile ./registry.json;
+      rendered = builtins.fromJSON raw;
+      nameValueList = map ({ from, ... }@value: {
+        inherit value;
+        name = from.id;
+      }) rendered.flakes;
+    in {
+      inherit raw rendered;
+      final = builtins.listToAttrs nameValueList;
+    };
+}
