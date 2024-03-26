6589ec91c639387575c0005c8dbafb807af5a2b3
João Nogueira <shires.waking0d@icloud.com>
2024-03-26 21:58:25 +0100

    Simplified it again

--- flake.nix
+++ flake.nix
@@ -1,14 +1,4 @@
 {
-  outputs = { ... }:
-    let
-      raw = builtins.readFile ./registry.json;
-      rendered = builtins.fromJSON raw;
-      nameValueList = map ({ from, ... }@value: {
-        inherit value;
-        name = from.id;
-      }) rendered.flakes;
-    in {
-      inherit raw rendered;
-      final = builtins.listToAttrs nameValueList;
-    };
+  description = "My custom registry";
+  outputs = { ... }: builtins.fromJSON (builtins.readFile ./registry.json);
 }
