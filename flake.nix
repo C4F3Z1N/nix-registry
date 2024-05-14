4aa02c55d61f6946ecab602a877f8b136d1cd0e6
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-05-14 12:04:23 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/44677a1c96810a8e8c4ffaeaad10c842402647c1?narHash=sha256-4pRuzsHZOW5W4CsXI9uhKtiJeQSUoe1d2M9mWU98HC4%3D' (2024-05-12)
      → 'github:nix-community/home-manager/6ebe7be2e67be7b9b54d61ce5704f6fb466c536f?narHash=sha256-b3/U21CJjCjJKmA9WqUbZGZgCvospO3ArOUTgJugkOY%3D' (2024-05-06)

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     # safely pinned to a ref (branch/tag);
+    home-manager.url = "github:nix-community/home-manager/6ebe7be2e67be7b9b54d61ce5704f6fb466c536f";
     nix.url = "github:nixos/nix/2.21.0";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
@@ -12,7 +13,6 @@
     flake-compat.url = "github:edolstra/flake-compat";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager";
     impermanence.url = "github:nix-community/impermanence";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     sops-nix.url = "github:mic92/sops-nix";
